08ff998c9e8d4069d7f4eb6bc0d5bacfcf04dceb
ajala ahmad <ahmadajala98@gmail.com>
2022-06-10 10:24:51 +0100

    Option does not exist exception class

---
+++ src/main/java/africa/semicolon/pollapplication/exceptions/OptionDoesNotExistException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.pollapplication.exceptions;
+
+public class OptionDoesNotExistException extends Throwable{
+    public OptionDoesNotExistException(String message){
+        super(message);
+    }
+}
