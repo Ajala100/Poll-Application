7f32d8b4b3b3898eb78d8491f8b23c98ab6af07f
ajala ahmad <ahmadajala98@gmail.com>
2022-06-17 15:30:58 +0100

    Option service interface

---
+++ src/main/java/africa/semicolon/pollapplication/service/OptionService.java
@@ -0,0 +1,7 @@
+package africa.semicolon.pollapplication.service;
+
+import africa.semicolon.pollapplication.data.models.Option;
+
+public interface OptionService {
+    Option createOption(String value);
+}
