facdf0c20f21698389f51f86b167ac28e5ee8dfe
ajala ahmad <ahmadajala98@gmail.com>
2022-06-19 07:32:19 +0100

    Poll dislike does not exist exception class

---
+++ src/main/java/africa/semicolon/pollapplication/exceptions/PollDislikeDoesNotExistException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.pollapplication.exceptions;
+
+public class PollDislikeDoesNotExistException extends Throwable{
+    public PollDislikeDoesNotExistException(String message){
+        super(message);
+    }
+}
