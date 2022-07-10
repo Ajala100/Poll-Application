e9fcaa0924101494db71795f71673729ee217174
ajala ahmad <ahmadajala98@gmail.com>
2022-07-10 15:52:40 +0100

    updated poll comment service interface

--- src/main/java/africa/semicolon/pollapplication/service/PollCommentService.java
+++ src/main/java/africa/semicolon/pollapplication/service/PollCommentService.java
@@ -3,16 +3,15 @@ package africa.semicolon.pollapplication.service;
 import africa.semicolon.pollapplication.exceptions.PollCommentDoesNotExist;
 import africa.semicolon.pollapplication.exceptions.PollDoesNotExistException;
 import africa.semicolon.pollapplication.exceptions.UserDoesNotExistException;
-import africa.semicolon.pollapplication.payLoad.request.CreatePollCommentRequest;
 import africa.semicolon.pollapplication.payLoad.response.CreatePollCommentResponse;
 import africa.semicolon.pollapplication.payLoad.response.PollCommentResponse;
 
 import java.util.List;
 
 public interface PollCommentService {
-    CreatePollCommentResponse createPollComment(CreatePollCommentRequest createPollCommentRequest) throws UserDoesNotExistException, PollDoesNotExistException;
-    void deletePollComment(Long pollId, Long commentId) throws PollDoesNotExistException, PollCommentDoesNotExist;
-    List<PollCommentResponse> viewAllCommentOnAPoll(Long pollId) throws PollCommentDoesNotExist, PollDoesNotExistException;
-    PollCommentResponse viewPostComment(Long pollId, Long commentId) throws PollDoesNotExistException, PollCommentDoesNotExist;
+    CreatePollCommentResponse createPollComment(String commenterId, String pollId, String comment) throws UserDoesNotExistException, PollDoesNotExistException;
+    void deletePollComment(String pollId, Long commentId) throws PollDoesNotExistException, PollCommentDoesNotExist;
+    List<PollCommentResponse> viewAllCommentOnAPoll(String pollId) throws PollCommentDoesNotExist, PollDoesNotExistException;
+    PollCommentResponse viewPollComment(String pollId, Long commentId) throws PollDoesNotExistException, PollCommentDoesNotExist;
 
 }
